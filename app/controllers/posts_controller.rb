1bfe6d162d18e88d90a96937f04c219cdf82431a
Mikaela Hammel <mikaela.hammel@kellogg.northwestern.edu>
2023-06-02 20:15:39 +0000

    codealong

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,20 @@ class PostsController < ApplicationController
 
   def index 
     @posts = Post.all.order("created_at desc")
+    posts_json = []
+    for post in @posts
+      user = User.find_by({ "id" => post["user_id"] })
+      posts_json << {
+        "username"=> user["username"],
+        "real_name" => user["real_name"],
+        "body" => post["body"],
+        "created_at" => post["created_at"]
+      }
+    end
+    respond_to do |format|
+      format.html
+      format.json { render :json => posts_json}
+    end
   end
 
   def create
